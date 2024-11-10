bd0a5c47a96704ce5da8679fe0a1829d1de57397
MimoMash <mimomashyt@gmail.com>
2024-11-10 17:43:56 +0100

    commit error, changes made same time battleshipscreen

--- game.mjs
+++ game.mjs
@@ -1,5 +1,5 @@
 import { ANSI } from "./utils/ansi.mjs";
-import { print, clearScreen, printCentered as printCentered } from "./utils/io.mjs";
+import { print, clearScreen, printCentered } from "./utils/io.mjs";
 import SplashScreen from "./game/splash.mjs";
 import { FIRST_PLAYER, SECOND_PLAYER } from "./consts.mjs";
 import createMenu from "./utils/menu.mjs";
@@ -7,26 +7,27 @@ import createMapLayoutScreen from "./game/mapLayoutScreen.mjs";
 import createInnBetweenScreen from "./game/innbetweenScreen.mjs";
 import createBattleshipScreen from "./game/battleshipsScreen.mjs";
 import { currentLanguage, setLanguage } from "./utils/language.mjs";
+import { GAME_BOARD_DIM } from "./consts.mjs";
+import { create2DArrayWithFill } from "./utils/array.mjs";
 
 const MAIN_MENU_ITEMS = buildMenu();
 const LANGUAGE_MENU_ITEMS = buildLangMenu();
 
-const GAME_FPS = 1000 / 60; // The theoretical refresh rate of our game engine
-let currentState = null;    // The current active state in our finite-state machine.
-let gameLoop = null;        // Variable that keeps a reference to the interval id assigned to our game loop 
+const GAME_FPS = 1000 / 60;
+let currentState = null;    
+let gameLoop = null;       
 let mainMenuScene = null;
 let languageMenu = null;
 
 (function initialize() {
-    setLanguage("no");
     resolutionCheck();
     print(ANSI.HIDE_CURSOR);
     clearScreen();
     mainMenuScene = createMenu(MAIN_MENU_ITEMS);
     languageMenu = createMenu(LANGUAGE_MENU_ITEMS);
     SplashScreen.next = mainMenuScene;
-    currentState = SplashScreen  // This is where we decide what state our finite-state machine will start in. 
-    gameLoop = setInterval(update, GAME_FPS); // The game is started.
+    currentState = SplashScreen;
+    gameLoop = setInterval(update, GAME_FPS); 
 })();
 
 function update() {
@@ -46,44 +47,53 @@ function resolutionCheck() {
     }
 }
 
-// Support / Utility functions ---------------------------------------------------------------
 
 function buildMenu() {
     let menuItemCount = 0;
     return [
         {
             text: currentLanguage.startGame,
-            id: menuItemCount++, 
+            id: menuItemCount++,
             action: function () {
                 clearScreen();
                 let inbetween = createInnBetweenScreen();
                 inbetween.init(currentLanguage.shipPlacement + "\n" +
-                                currentLanguage.getReadyP1 + "\n" +
-                                currentLanguage.lookAwayP2, () => {
-
-                let p1map = createMapLayoutScreen();
-                p1map.init(FIRST_PLAYER, (player1ShipMap) => {
-
-
-                    let inbetween = createInnBetweenScreen();
-                    inbetween.init(currentLanguage.shipPlacement + "\n" +
-                                currentLanguage.getReadyP1 + "\n" +
-                                currentLanguage.lookAwayP1, () => {
-                        let p2map = createMapLayoutScreen();
-                        p2map.init(SECOND_PLAYER, (player2ShipMap) => {
-                        return createBattleshipScreen(player1ShipMap, player2ShipMap);
-                        })
-                    return p2map;
-
+                    currentLanguage.getReadyP1 + "\n" +
+                    currentLanguage.lookAwayP2, () => {
+        
+                    let p1map = createMapLayoutScreen();
+                    let firstPlayerBoard = null;
+                    
+                    p1map.init(FIRST_PLAYER, (player1Map) => {
+                        firstPlayerBoard = {
+                            ships: player1Map,
+                            target: create2DArrayWithFill(GAME_BOARD_DIM),
+                        };
+        
+                        let inbetween = createInnBetweenScreen();
+                        inbetween.init(currentLanguage.shipPlacement + "\n" +
+                            currentLanguage.getReadyP2 + "\n" +
+                            currentLanguage.lookAwayP1, () => {
+                            
+                            let p2map = createMapLayoutScreen();
+                            p2map.init(SECOND_PLAYER, (player2Map) => {
+                                let secondPlayerBoard = {
+                                    ships: player2Map,
+                                    target: create2DArrayWithFill(GAME_BOARD_DIM),
+                                };
+                                
+                                let battleshipScreen = createBattleshipScreen();
+                                battleshipScreen.init(firstPlayerBoard, secondPlayerBoard);
+                                return battleshipScreen;
+                            });
+                            return p2map;
+                        });
+                        return inbetween;
                     });
-                return inbetween;
-
-                });
-            return p1map;
-
-            }, 3);
-            currentState.next = inbetween;
-            currentState.transitionTo = "Map layout";
+                    return p1map;
+                }, 2);
+                currentState.next = inbetween;
+                currentState.transitionTo = "Map layout";
             }
         },
 
@@ -117,19 +127,19 @@ function buildLangMenu() {
                 mainMenuScene = createMenu(buildMenu());
                 currentState.next = mainMenuScene;
                 currentState.transitionTo = "Main Menu";
-                clearScreen();
             }
         },
         {
             text: currentLanguage.langNorwegian, 
-            id: menuItemCount++, 
+            id: menuItemCount++,
             action: function() {
                 setLanguage("no");
                 mainMenuScene = createMenu(buildMenu());
                 currentState.next = mainMenuScene;
                 currentState.transitionTo = "Main Menu";
-                clearScreen();
             }
         }
     ];
 }
+
+export default buildMenu;
