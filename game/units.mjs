4c28510b325b6f6e66cc2d1976f62f8e2ca0ade2
MimoMash <mimomashyt@gmail.com>
2024-11-10 09:39:31 +0100

    added language support

--- game/units.mjs
+++ game/units.mjs
@@ -1,9 +1,11 @@
+import { currentLanguage } from "../utils/language.mjs"
+
 const units = {
-    carrier: { size: 5, id: "Carrier", symbole: "O" },
-    battleship: { size: 4, id: "Battleship", symbole: "K" },
-    cruiser: { size: 3, id: "Cruiser", symbole: "T" },
-    submarine: { size: 3, id: "Submarine", symbole: "X" },
-    destroyer: { size: 2, id: "Destroyer", symbole: "Q" }
+    carrier: { size: 5, id: currentLanguage.carrier, symbol: "O" },
+    battleship: { size: 4, id: currentLanguage.battleship, symbol: "K" },
+    cruiser: { size: 3, id: currentLanguage.cruiser, symbol: "T" },
+    submarine: { size: 3, id: currentLanguage.submarine, symbol: "X" },
+    destroyer: { size: 2, id: currentLanguage.destroyer, symbol: "Q" }
 }
 
 
